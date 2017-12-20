c822fec8955218e47c5378e3e4899898d062c1fb
Daniel Molina <daniel.molina@uca.es>
2017-12-20 15:27:57 +0100

    Feature: Add list of algorithms.

--- static_web/run.js
+++ static_web/run.js
@@ -3,9 +3,10 @@ var app = new Vue({
     data: {
         benchmarks: {},
         selected: '',
-        algs: {},
-        sel_algs: [],
-        error: ''
+        available_algs: {},
+        algs: [],
+        error: '',
+        dimension: ''
     },
     mounted: function() {
         var self = this;
@@ -20,11 +21,16 @@ var app = new Vue({
     methods: {
         onChangedBenchmark: function() {
             var self = this;
+            var dimensions = self.benchmarks[self.selected].dimensions;
+
+            if (length(dimensions)==1) {
+                self.dimension = dimensions[0];
+            }
             $.ajax({
                 url: 'http://localhost:5000/algs/' +self.selected,
                 method: 'GET',
                 sucess: function(data) {
-                    self.algs = data['algs'];
+                    self.available_algs = data['algs'];
                     self.error = data['error'];
                 }
             });
