50b97095e8193c2e0363ac4831d2350515458954
Daniel Molina <daniel.molina@uca.es>
2017-12-29 13:50:41 +0100

    Fix: error with not selected Benchmark.

--- static_web/runcommon.js
+++ static_web/runcommon.js
@@ -1,5 +1,5 @@
 Vue.component('select-bench', {
-    template: '<div><label>Benchmark</label><select v-model="selected" v-on:change="changed">\
+    template: '<div><label>Select the Benchmark </label><select v-model="selected" v-on:change="changed">\
         <option value="" selected="selected" value="-1">------</option>\
         <option v-for="bench in benchmarks" :value="bench.id">{{bench.name}}</option>\
         </select></div>',
@@ -20,12 +20,43 @@ Vue.component('select-bench', {
     methods: {
         changed: function() {
             var self = this;
-            var bench = self.benchmarks[self.selected];
-            this.$emit('update:benchmark', bench);
-            console.log(self.benchmark);
+
+            if (self.selected < 0) {
+                this.$emit('update:benchmark', {});
+            }
+            else {
+                var bench = self.benchmarks[self.selected];
+                this.$emit('update:benchmark', bench);
+            }
         }
     }
 });
+
+
+Vue.component('input-alg', {
+    template: '<div>\
+The required format is indicated <a v-bind:href="example">here</a>.\
+</p>\
+<label id="file" for="fileupload">Select a file (.csv or .xls) to upload<br/></label>\
+<p><input id="upload_button" type="file" name="file"></p>\
+</div>',
+    props: ['benchmark'],
+    computed: {
+        example:  function(){
+            var self = this;
+            var example = self.benchmark.example;
+
+            if (example.length > 0) {
+                return "examples/" +example +".xls";
+            }
+            else {
+                return '#'
+            }
+
+        }
+    }
+}
+);
 
 var make_ajax_info = function(name, e) {
     e.preventDefault();
